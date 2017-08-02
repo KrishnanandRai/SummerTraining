b62eec252e7a5ad4d3471b9e37673e06ba173fae
Krishnanand Rai <knraikaip@gmail.com>
2017-08-02 23:58:32 +0530

    Add files via upload

---
+++ checkAttendance.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import urllib.request
+
+studentPresent = set()
+
+url = str(input("Enter url of log"))
+with urllib.request.urlopen(url) as attendanceList:
+	for line in attendanceList:
+		line = line.decode('utf-8')
+		data = line.split()
+		data[1] = data[1].strip('b<>')
+		studentPresent.add(data[1])
+studentPresent.remove('CLASS----')
+
+nick = input("Enter a nick to check attendence")
+if nick in studentPresent:
+	print("YES " + nick + " is present")
+else:
+	print(nick + " is not present")
+
+print("list of all the student present today is:")
+print(studentPresent)		
+
+
+
+ 
