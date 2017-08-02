76f2064d47d435f2f6fac51747b3f20e9b88a013
Krishnanand Rai <knraikaip@gmail.com>
2017-08-03 00:50:23 +0530

    Update checkAttendance.py

--- checkAttendance.py
+++ checkAttendance.py
@@ -3,7 +3,7 @@ import urllib.request
 
 studentPresent = set()
 
-url = str(input("Enter url of log "))
+url = str(input("Enter url of log:  "))
 with urllib.request.urlopen(url) as attendanceList:
 	for line in attendanceList:
 		line = line.decode('utf-8')
@@ -12,12 +12,14 @@ with urllib.request.urlopen(url) as attendanceList:
 		studentPresent.add(data[1])
 studentPresent.remove('CLASS----')
 
-nick = input("Enter a nick to check attendence: ")
+nick = input("Enter a nick to check attendence:  ")
 if nick in studentPresent:
 	print("YES " + nick + " is present")
 else:
 	print(nick + " is not present")
-
+	
+print(" ")
+print(" ")
 print("list of all the student present today is:")
 print(studentPresent)		
 
